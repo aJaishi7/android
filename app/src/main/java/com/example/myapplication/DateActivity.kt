c4f7d0aa83102dcebc1702a0d3777680900845bd
Arjun Jaishi <a.jaishi7777777@gmail.com>
2020-12-13 13:00:20 +0545

    Date Activity

--- app/src/main/java/com/example/myapplication/DateActivity.kt
+++ app/src/main/java/com/example/myapplication/DateActivity.kt
@@ -1,15 +1,49 @@
 package com.example.myapplication
 
+import android.app.DatePickerDialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.DatePicker
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import java.util.*
 
 class DateActivity : AppCompatActivity() {
+    private lateinit var tvDate: TextView;
+    private lateinit var btnLoad: Button;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_date)
+        tvDate = findViewById(R.id.tvDate);
+        btnLoad = findViewById(R.id.btnLoad);
+
+        btnLoad.setOnClickListener {
+            loadCalander();
+        }
+
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun loadCalander() {
+        var calendar = Calendar.getInstance();
+        var day: Int = calendar.get(Calendar.DAY_OF_MONTH);
+        var month: Int = calendar.get(Calendar.MONTH);
+        var year: Int = calendar.get(Calendar.YEAR);
 
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { view, year, month, dayOfMonth -> tvDate.text = "Date: $day/$month/$year" },
+            year,
+            month,
+            day
+        );
+        datePickerDialog.show();
+
+
+    }
 
 }
